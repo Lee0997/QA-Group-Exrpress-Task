96714e849cd0723acf08efbf2245e11194d3c3b7
junaidaziz68 <junaidaziz68@outlook.com>
2022-05-05 14:29:10 +0100

    ja:finsihed routing to controller

--- api/v1/route/todo.router.js
+++ api/v1/route/todo.router.js
@@ -1,31 +1,18 @@
 const express= require('express');
-const todo = require('./model/todo');
+const todoController = require('../controller/todo-controller')
+
 const router = express.Router();
 
 
 
+router.get('/',todo-Controller.readAll);
 
-router.get('/', (req, res) => {
-    res.status(200).json();
-});
+router.get('/',todo-controller.readById);
 
+router.post('/create',todo-controller.create);
 
-router.get('/', (req, res) => {
-    res.status(200).json();
-});
+router.put('/update', todo-controller.update);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.delete('/delete', todo-controller.delete);
 
 module.exports = router;

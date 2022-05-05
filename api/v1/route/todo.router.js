e87c54f9caf1d03e7f486182b4e68340a3b9c013
junaidaziz68 <junaidaziz68@outlook.com>
2022-05-05 14:15:03 +0100

    ja

--- api/v1/route/todo.router.js
+++ api/v1/route/todo.router.js
@@ -1,2 +1,31 @@
 const express= require('express');
+const todo = require('./model/todo');
 const router = express.Router();
+
+
+
+
+router.get('/', (req, res) => {
+    res.status(200).json();
+});
+
+
+router.get('/', (req, res) => {
+    res.status(200).json();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;

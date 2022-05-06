b6bad1bfd49147e9d14591b2c3c10176059cbd81
Lee <leenock2012@yahoo.co.uk>
2022-05-06 15:00:06 +0100

    Update todo.js
    Refactored for use with mongoose

--- api/v1/model/todo.js
+++ api/v1/model/todo.js
@@ -1,5 +1,17 @@
-module.exports = function Todo(id, name, todo) {
-    this.id = id;
-    this.name = name;
-    this.todo = todo;
-}
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+  name: String,
+  todo: String,
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
+
+// module.exports = function Todo(id, name, todo) {
+//     this.id = id;
+//     this.name = name;
+//     this.todo = todo;
+// }
